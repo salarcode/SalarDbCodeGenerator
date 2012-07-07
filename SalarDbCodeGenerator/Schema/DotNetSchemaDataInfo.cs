715df4e22b0c01173ab21cfc9600f229170456fa
salar2k <none@none.com>
2012-07-07 11:27:29 +0000

    Version 2.0 pre-release. This version is not compatible with previous one. * New database schema engine. * New pattern schema engine. * New generator engine. * All patterns are updated. * New project pattern schema. * Invalid column name detection is improved. * Generator overall performance improvement. * NHibernate MappingByCode simple and MVVM patterns are added.

---
+++ SalarDbCodeGenerator/Schema/DotNetSchemaDataInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SalarDbCodeGenerator.Schema.Patterns;
+
+namespace SalarDbCodeGenerator.Schema
+{
+	public class DotNetSchemaDataInfo
+	{
+		public const string DotNetArrayIdenticator = "[]";
+
+		public static bool IsStringType(string dotNetDataType)
+		{
+			if (dotNetDataType == "System.String")
+				return true;
+			return false;
+		}
+		public static string DetermineNumericType(string dotNetDataType)
+		{
+			switch (dotNetDataType)
+			{
+				case "System.Int16":
+					return ConditionKeyModeConsts.FieldNumericType.Integer;
+				case "System.Int32":
+					return ConditionKeyModeConsts.FieldNumericType.Integer;
+				case "System.Int64":
+					return ConditionKeyModeConsts.FieldNumericType.Integer;
+				case "System.Byte":
+					return ConditionKeyModeConsts.FieldNumericType.Integer;
+				case "System.Decimal":
+					return ConditionKeyModeConsts.FieldNumericType.Decimal;
+				case "System.Single":
+					return ConditionKeyModeConsts.FieldNumericType.Decimal;
+				case "System.Double":
+					return ConditionKeyModeConsts.FieldNumericType.Decimal;
+ 			}
+			return "";
+		}
+
+	}
+}
